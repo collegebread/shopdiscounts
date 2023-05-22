1227c1a49f88a8c57d39af3fdada3cc787269652
Dev <57229351+antwk@users.noreply.github.com>
2023-05-22 09:42:20 -0700

    update footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,31 +1,33 @@
- <!-- Footer -->
- <footer class="footer">
-      <div class="footer-about">
+   <!-- Footer -->
+   <footer>
+      <div class="about">
         <h3>About</h3>
-        <ul class="footer-links">
+        <ul>
           <li><a href="#">Become a Partner</a></li>
-      </div>
-      <div class="footer-legal">
+          </ul>
+          </div>
+          <div class="legal">
         <h3>Legal</h3>
-        <ul class="footer-links">
+        <ul>
           <li><a href="#">Terms of Use</a></li>
           <li><a href="#">Privacy Policy</a></li>
           <li><a href="#">Cookie Policy</a></li>
         </ul>
-      </div>
-      <div class="footer-more">
+        </div>
+        <div class="redirect">
         <h3>More</h3>
         <ul>
           <li><a href="#">Black Friday</a></li>
         </ul>
-      </div>
-      <div class="footer-follow">
+        </div>
+        <div class="social">
         <h3>Follow Us</h3>
-        <ul class="social-links">
+        <ul>
           <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
           <li><a href="#"><i class="fab fa-twitter"></i></a></li>
           <li><a href="#"><i class="fab fa-instagram"></i></a></li>
           <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
         </ul>
-      </div>
-    </footer>
+        <hr class="copyright">  <p>Wordpress All rights reserved</p>
+        </div>
+        </footer>
