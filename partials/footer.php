3461ef9bb8e6920b9b7a20dbf6532be13a326a3d
Dev <57229351+antwk@users.noreply.github.com>
2023-05-22 10:08:55 -0700

    update footer

--- partials/footer.php
+++ partials/footer.php
@@ -23,10 +23,10 @@
         <div class="social">
         <h3>Follow Us</h3>
         <ul>
-          <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+          <li><a href="pages/404page.html"><i class="fab fa-facebook-f"></i></a></li>
+          <li><a href="pages/404page.html"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="pages/404page.html"><i class="fab fa-instagram"></i></a></li>
+          <li><a href="pages/404page.html"><i class="fab fa-linkedin-in"></i></a></li>
         </ul>
         <hr class="copyright">  <p>Wordpress All rights reserved</p>
         </div>
