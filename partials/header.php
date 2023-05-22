8ba7587b9f8541135fa50210c9f634a7deaaa894
Dev <57229351+antwk@users.noreply.github.com>
2023-05-21 20:06:29 -0700

    update header.php

--- partials/header.php
+++ partials/header.php
@@ -1,42 +1,39 @@
 <header class="above-header">
     <!-- Add content for the above header here -->
+<!--    
+      <h4 class="above-header description">Act fast and save big on your favorite brands with our coupons and promo codes</h4>
+ 
+    </header> -->
    
-  </header>
-
   <!-- Main header -->
   <header class="main-header">
     <!-- Add content for the main header here -->
-  </header>
-
-  <nav class="navbar">
-    <a href="#" class="navbar-brand">Dealhunter</a>
-    <form class="search-form" aria-label="Search Coupons, Deals, and More">
-      <input type="text" placeholder="Search Coupons, Deals, and More">
+     <nav class="nav">
+      <div class="brand-container">
+    <a href="index.html" class="logo">Dabbl</a> </div>
+    <div class="search-container">
+    <form class="search-form" aria-label="Search Coupon Discounts and Deals">
+      <input type="text" placeholder= "Search by Product or Store">
       <button type="submit"><i class="fas fa-search"></i></button>
     </form>
-    <button class="navbar-toggle" aria-label="Toggle Navigation" onclick="toggleNav()">
-      <i class="fas fa-bars"></i>
-    </button>
-    <ul class="nav-links" aria-labelledby="navbar-brand">
-      <li class="button-blue"><a href="#"><i class="fas fa-puzzle-piece"></i> Addon Web Extension</a></li>
-      <li id="button-green"><a href="#"><i class="fas fa-heart"></i> Wishlist</a></li>
-      <li><a href="#"><i class="fas fa-bell"></i> Alerts</a></li>
-      <li><a href="#"><i class="fas fa-user-plus"></i> Sign Up</a></li>
+    </div>
+    <ul class="nav-menu" aria-labelledby="navbar-brand">
+      <li class="nav-object"><a href="#"><i class="fas fa-puzzle-piece"></i></a></li>
+      <li class="nav-object"><a href="#"><i class="fas fa-share-alt"></i></a></li>
+      <li class="nav-object"><a href="#"><i class="fas fa-bell"></i></a></li>
+      <li class="nav-item"><a class="nav-link" href="pages/login.html">Login </a></li>
+      <li class="nav-item"><a class="nav-link" href="pages/sign-up.html"> Sign Up</a></li>
+    </ul>
+    <hr style="width: 100%;" background-color="black">
+  <div class="subnav">
+    <ul class="subnav-menu" aria-labelledby="sub-nav-header">
+      <li class="subnav-link"><a class="inactive" href="#">Trending</a></li>
+      <li class="subnav-link"><a href="#">Top Coupons</a></li>
+      <li class="subnav-link"><a href="#">Shoes </a></li>
+      <li class="subnav-link"><a href="pages/women's-fashion-deals.html">Women's Fashion</a></li>
+      <li class="subnav-link"><a href="#">Men's Fashion</a></li>
+      <li class="subnav-link"><a href="#">Electronics</a></li>
+      <li class="subnav-link"><a href="#">Video Games</a></li>
+      <li class="subnav-link"><a href="#">Babies and Kids</a></li>
     </ul>
   </nav>
-
-  <div class="sub-nav">
-    <ul class="sub-nav-links" aria-labelledby="sub-nav-header">
-      <li><a href="#">Trending</a></li>
-      <li><a href="#">Top Coupons</a></li>
-      <li><a href="#">Women's Fashion</a></li>
-      <li><a href="#">Men's Fashion</a></li>
-      <li><a href="#">Beauty and Health</a></li>
-      <li><a href="#">Food & Drink</a></li>
-      <li><a href="#">Video Games</a></li>
-      <li><a href="#">Tech</a></li>
-      <li><a href="#">Babies and Kids</a></li>
-    </ul>
-    </div>
-
-    
