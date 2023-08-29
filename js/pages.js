d0a35ecb597127fab37f711af2763d0108569edf
Dev <57229351+antwk@users.noreply.github.com>
2023-08-29 17:45:29 -0400

    update page scripts

--- js/pages.js
+++ js/pages.js
@@ -1,13 +1,16 @@
+// JavaScript code for scroll event
 window.addEventListener("scroll", function() {
-    const nav = document.querySelector(".nav.nav--navbar");
-    if (window.scrollY > 0) {
-      nav.classList.add("scrolled");
-    } else {
-      nav.classList.remove("scrolled");
-    }
+  const nav = document.querySelector(".nav.nav--navbar");
+  if (window.scrollY > 0) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
+// jQuery code for sidebar toggle
+$(document).ready(function () {
+  $('.dropdown-arrow.sidebar--toggler').click(function () {
+    $(this).closest('.sidebar_col-md').toggleClass('active');
   });
-  $(document).ready(function () {
-    $('.dropdown-arrow sidebar--toggler').click(function () {
-      $(this).closest('.sidebar_col-md').toggleClass('active');
-    });
-  });
+});
